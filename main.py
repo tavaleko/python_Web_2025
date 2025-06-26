bb28f16829c6c5b96fe65aaf656dfe7da216ee72
LCIMS1 <tye@mk.ru>
2025-06-26 10:03:43 +0300

    commit

---
+++ main.py
@@ -0,0 +1,50 @@
+
+print('mause house "ville"')
+# python enhanced proposal 8
+name = 'Сергей' # string
+surname = 'Петров'
+age = 27
+print('Привет,', name, surname, 'тебе', age, 'лет')
+temperatura = 27
+print('На улице температура,', temperatura, 'градусов.')
+name = input('как тебя зовут: ') # str
+print('Привет,', name)
+print('Приятно познакомиться')
+print()
+
+donut = int(input('стоимость пончика: '))
+cofee = int(input('Стоимость кофе: '))
+#  total = coffee + donut
+# int, float, str
+print('С вас', cofee + donut, 'руб.')
+donut = input('стоимость пончика: ')
+cofee = input('Стоимость кофе: ')
+#  total = coffee + donut
+# int, float, str
+print('С вас', int(cofee) + int(donut), 'руб.')
+donut = float(input('стоимость пончика: '))
+cofee = float(input('Стоимость кофе: '))
+#  total = coffee + donut
+# int, float, str
+print('С вас', cofee + donut, 'руб.')
+a = 35.999
+print(int(a))
+temp = round(a)
+temp = round(a,2)
+temp = round(a,0)
+print(temp)
+screen = 1280 //6
+print(screen)
+number = int(input('Введите число: '))
+print('Число', number, 'заканчивается на', number % 10)
+print('Число'+ str(number) + 'заканчивается на', number % 10)
+""" 
+у лукоморья дуб зеленый
+"""
+temp = """ 
+у лукоморья дуб зеленый
+"""
+number = int(input('Введите число: '))
+print( 'число',number, 'в степени 3 будет', number**3 )
+print( 'квадратный корень от', number, 'будет', number **(1/2))
+print(4/2)# всегда будет float
