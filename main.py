56a26de97007e50a0b445f35f5cd21e19ee48571
LCIMS1 <tye@mk.ru>
2025-07-04 12:58:11 +0300

    lesson_8 (full lesson) import библиотек math,random, datetime,-Python Imagine Library

--- main.py
+++ main.py
@@ -46,16 +46,128 @@
 # print('Синус 30:', round(sin(radians(30)), 2))# Синус 30: 0.5
 # print('Гипотинуза для 3 и 2: ', hypot(3,2))# Гипотинуза для 3 и 2:  3.605551275463989
 #################################
-# # Модуль рандом
+# # Модуль рандом встроеный модуль
 ##############################
 # import random as r
 #
 # for _ in range(10):
 #     print(r.randint(0,10))
 #     #print(r.randrange(0,10,2))
+# import random as r
+# lst = [1,2,3,45,6,7,8,9]
+# res = r.choice(lst)
+# print(res) # вывел 8 но каждый раз будет выводить разное число от 1 дл 9
+# print(r.choice('орёл'))# в строке выбирает любые буквы
+# не работает со словарями и множествами!!!!
+# d = {'a': 1,
+#      'b':2,
+#      'c':3,
+# }
+# keys = list(d.keys())# перевели в лист
+# key = r.choice(keys)
+# print(d[key]) # на прямую к словарю обратиться нельзя
+#
+# zara = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685',]
+# for _ in range(10):
+#     print(r.choice(zara), r.choice(zara)) # бросаем кубики выведет кубики
+#################################################
+# choice и sample отличия sample будет без повторов
+#############################################
+# lst = [1,2,3,45,6,7,8,9]
+# res = r.sample(lst, k=5)# вторая часть пишется через именованый элемент обязательно!
+# print(res) # вывел [8, 3, 7, 1, 6]
+# for _ in range(10):
+#     print(r.sample(lst, k=5))# каждый раз выводит разные, но уникальные числа без повторов
+# ####################################################
+# # shuffle перетосовать
+############################################
+# abc = 'qwerrytyuiopasdfghhjklzxcvbnm'
+#
+# lst1 = list(abc)
+# r.shuffle(lst1)
+# print(lst1)
+# abc = 'qwerrytyuiopasdfghhjklzxcvbnm'
+#
+# lst1 = list(abc)+ ['1','2']+['#','$']
+# r.shuffle(lst1)
+# res = ''.join(lst1[:8])
+# print(res)
+############ задача сгенерировать пароль!!!!
+# N = 8
+# abc = 'qwerytyuiopasdfghhjklzxcvbnm'
+# num ='1234567890'
+# spec ='@#$&'
+#
+# abc = list(abc)# переводим в лист
+# num = list(num)
+# spec = list(spec)
+#
+# r.shuffle(abc)
+#
+# temp = abc[:N - 3]# вычитаем 3 символа под спец символы
+# temp.append(r.choice(abc).upper())# переводим в Верхний регистр для большой буквы
+# temp.append(r.choice(num))
+# temp.append(r.choice(spec))
+# r.shuffle(temp)
+# res = ''.join(temp)# объеденяем
+# print(res)
+#############################################################
+# seed разобраться самим
+# r.seed()
+# print(r.random()) # 0.4842436831029522 всегда разное число
+#########################################
+# data time - встроеные часы нашего компьютора
+########################################
+# import datetime as dt
+# from time import strftime
+#
+# print(dt.datetime.now())# 2025-07-04 12:24:30.189750 сейчас по мировому стандарту
+# print(dt.datetime.now().date())#2025-07-04
+# print(dt.datetime.now().time())#12:25:56.487988
 
 
-
-
-
-
+#######################################
+# # strftime встроеный модуль
+##########################################
+# time =dt.datetime.now()
+#
+# ftime = time.strftime('%d')
+#
+# print(ftime)# получили что сегодня 4 число
+# ftime = time.strftime('%d-%m-%y')
+# print(ftime)# 04-07-25
+# ftime = time.strftime('%d/%m/%y')
+# print(ftime)# 04/07/25
+# ftimeh = time.strftime('%H:%M')
+#
+# print('Сегодня: ', ftime)#Сегодня:  04/07/25
+# print('Время: ', ftimeh)# Время:  12:32
+# # время заказа
+# my_time = dt.time(15,27,35)
+# print(my_time)#15:27:35 не обязательно писать секунды
+# my_date =dt.date(2025,7,4)
+# print(my_date)#2025-07-04 стандартное написение через дефис!!!!
+# my_day_time = dt.datetime.combine(my_date,my_time)
+# print(my_day_time)#2025-07-04 15:27:35
+# date1 = dt.date(2025,6,15)
+# date2 = dt.date(2025,7,3)
+# delta = date1-date2
+# print(delta)# -18 days, 0:00:00
+# print(dir(dt))
+# #'date', 'datetime', 'datetime_CAPI', 'time', 'timedelta', 'timezone', 'tzinfo' методы datetime есть
+# # обривиатура месяца
+#############################################
+## pprint еще один встроеный модуль
+############################################
+# from pprint import pp
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+# ]
+# print(matrix)
+# pp(matrix)
+##############################################
+## ВНЕШНИЕ БИБЛИОТЕКИ
+##Графика
+## PIL -Python Imagine Library
