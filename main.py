9bdf5c3dfd5c2966968115155ec432585d2de277
LCIMS1 <tye@mk.ru>
2025-07-07 11:31:27 +0300

    lesson_9 (full lesson) convert. docx

--- main.py
+++ main.py
@@ -32,4 +32,77 @@
 # draw.text((x, y), text, fill=(255, 255, 0), font=font)
 #
 # image.save('images/sunny_day.jpg')
+#################################################
+#from PIL import Image, ImageDraw, ImageFont
 
+# convert and paste
+################################################
+# orgig = Image.open('images/sunny_day.jpg').convert('RGB')
+# up = orig.crop((0,0,600,200)) # половина изображения верхня часть
+# down = orig.crop((0,200,600,400))
+# new = Image.new('RGB',(600,400))
+# new.paste(down, (0,0))# верхний левый угол с этими координатами
+# new.paste(down, (0,0))
+
+# new.show()
+# from PIL Image, ImageFilter, ImageEnhance
+# orig = Image.open('images/kukla-labubu.webp')
+# #размытие
+# blu_image = orig.filter(Image)# дописать скопирорвать
+
+#Усиление резкости
+# enchacer = ImageEnhance.Sharpness(orig)#ДОПИСАТЬ
+
+
+# edges = orig.filter(ImageFilter.FIND_EDGES)# ДОПИСАТЬ
+# edges.show()
+# #############################
+#Документы
+# word - DOCX(python-docx)
+#pip freeze > requirement.txt
+# pip install -r requirement.txt
+###########p##############################
+# документы по шаблону(template.docx)
+from docx import Document
+# from docx.enum.text import WD_ALING_PARAGRAPH
+# from docx.shared import Cm, Inches, Mm
+# # Добавим в заголовок
+# doc = Document()# создание документа
+# doc.add_heading('Отчёт за месяц', 1)
+# paragraph = doc.add_paragraph('В этом отчёте представлены')
+# paragraph.add_run('ключевые показатели')
+# doc.save('docs/report.docx')
+# #run - это что то внутри обзаца
+# paragraph = doc.add_paragraph()
+# paragraph_format = paragraph.paragraph_format
+# paragraph_format.aligment = WD_ALING_PARAGRAPH.LEFT
+#
+# paragraph = doc.add_paragraph('Первый пункт', style = 'List Bulet')
+# paragraph = doc.add_paragraph('Второй пункт', style = 'List Bulet')
+#
+# paragraph = doc.add_paragraph('Первый пункт', style = 'List Number')
+# paragraph = doc.add_paragraph('Второй пункт', style = 'List Number')
+# paragraph = doc.add_paragraph()
+# table = doc.add_table(row = 3, cols=3)
+#
+# for i, row in enumerate(table.rows):
+#     for j, cell in enumerate(table.columns):
+#         cell.text = f'Строка {i+1} Столбец {j+1}'
+#
+# doc.add_paragraph()
+# doc.add_picture('images/sunny_day.jpg',width = Mm(100))
+# doc.save('docs/report.docx')
+from docxtpl import DocxTemplate
+
+#Загрузка шаблона
+doc = DocxTemplate('docs/template.docx')
+#Word - Docx(docxtpl)
+
+content = {
+    'company': 'ООО Монолит',
+    'employee': 'Петров Д.И',
+    'position': 'Менеджер',
+    'date':'01/01/2025',
+}
+doc.render(content)
+doc.save('docs/about.docx')
