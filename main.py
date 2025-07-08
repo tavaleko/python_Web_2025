eeb6cc94ea6932618221d07674f111f35b943435
LCIMS1 <tye@mk.ru>
2025-07-08 12:57:48 +0300

    lesson_10 (full lesson) pickle, project dirs,try-except, raise, исключения assertions,

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+from operator import index
 # txt =[4,2,4,6,3,5,9,7]
 # txt1 =[5,1,8,2,6,8,1,2]
 # txt2 =txt + txt1
@@ -67,12 +66,190 @@ from tkinter.font import names
 #     fo = open('information.txt')
 # except FileNotFoundError:
 #     print('Такого файла нет')#Такого файла нет то есть в случае исключения можно записать ошибку и она выдаваться не будет
+#
+# try:
+#     fo = open('information.txt')
+#     print(fo.read())
+#     fo.close()
+# except FileNotFoundError:# веток ексепт может быть сколько угодно
+#     print('Файл не обноружен  и создан с парометрами по умолчанию')#
+#     with open('information.txt','wt',encoding='utf-8') as fo:
+#         fo.write('По умолчанию')
+# ######################################
+# # Исключения
+# # try:
+# #     что пытаемся сделать
+# # except:
+# #     обрабатываем исключения
+# # else:
+# #      ветка которая отработает если исключений не было
+# # finally:
+# #     выполняется в любом случае
+#########################################
+# Полное написание при необходимости если открываем файл то надо делать его в обертке try-except или  finally
+# только try использовать нельзя
+# try:
+#     fo = open('information.txt')
+#     print(fo.read())
+#     fo.close()
+# except FileNotFoundError:# веток ексепт может быть сколько угодно
+#     print('Файл не обноружен  и создан с парометрами по умолчанию')#
+#     with open('information.txt','wt',encoding='utf-8') as fo:
+#         fo.write('По умолчанию')
+# else:
+#     print('Файл открыт успешно. Читаем его и закрываем')
+#     print(fo.read())
+#     fo.close()
+# finally:
+#     print('Продолжаем работать')
+######################################################
+# Исключения они runtime в процессе работы
+# flag = False# открывался ли на запись
+# try:
+#   fo = open('informations.txt', encoding='utf-8')
+#   print(fo.read())
+#
+# except FileNotFoundError:# веток ексепт может быть сколько угодно
+#     open('informations.txt', 'wt', encoding='utf-8')
+#     flag = True
+#     print('Файл не обноружен  и создан с парометрами по умолчанию')#
+#
+# else:
+#     print('Файл открыт успешно. Читаем его и закрываем')
+#     print(fo.read())
+#     fo.close()
+# finally:
+#     if flag:
+#         fo.write('По умолчанию')
+#         fo.close()
+#         print('Продалжаем работать')
+###########################################################
+# print('Остаток от деления')
+#
+# try:
+#     value = int(input('На что делим число 10:'))
+#     res = 10% value
+#     print(f'Остаток от деления 10 на {value} = {res}')
+# except ZeroDivisionError:
+#     print('На ноль делить нельзя!')
+# except ValueError:
+#     print('Надо вводить только целые числа')
+# except Exception as exp:
+#     print('Произошло исключение:', exp)
+#     print('Остаток от деления')
+# print('Остаток от деления')
+# try:
+#     value = int(input('На что делим число 10:'))
+#     res = 10 % value
+#     print(f'Остаток от деления 10 на {value} = {res}')
+# except ZeroDivisionError:
+#     print('На ноль делить нельзя!')
+#
+# except Exception as exp:
+#     print('Произошло исключение:', exp.__class__.__name__)
 
-try:
-    fo = open('information.txt')
-    print(fo.read())
-    fo.close()
-except FileNotFoundError:
-    print('Файл не обноружен  и создан с парометрами по умолчанию')#
-    with open('information.txt','wt',encoding='utf-8') as fo:
-        fo.write('По умолчанию')
+# print('Остаток от деления')
+# while ( value := int(input('На что делим число 10:'))):
+#     try:
+#         res = 10 % value
+#         print(f'Остаток от деления 10 на {value} = {res}')
+#     except ZeroDivisionError:
+#         print('На ноль делить нельзя!')
+#
+#     except Exception as exp:
+#         print('Произошло исключение:', exp.__class__.__name__)
+
+#
+# print('Остаток от деления')
+# flag = True
+# while flag:
+#     try:
+#         value = int(input('На что делим число 10:'))
+#         res = 10 % value
+#         print(f'Остаток от деления 10 на {value} = {res}')
+#     except ZeroDivisionError:
+#         print('На ноль делить нельзя!')
+#
+#     except Exception as exp:
+#         print('Произошло исключение:', exp.__class__.__name__)
+#     else:
+#         flag = False
+###################################################
+# # "Бросаемся исключениями" - throw в питоне raise
+#####################################################
+# max_val = 10
+# min_val = 1
+#
+# try:
+#     val = int(input(f'Введите целое число от {min_val} до {max_val}: '))
+#     if not min_val < val < max_val:
+#         raise ValueError ('введеное число вне диапазона ')
+#     print(f'Введеное число {val} лежит в задоном диапазоне')
+# except ValueError as exp:
+#     print('Надо быть внимательнее:',exp)
+#########################################
+# Для целей отладки используют утверждения (assertions)
+# В основном для нужд тестирования
+#########################################
+# try:
+#     text = input('Введите текст: ')
+#     assert  len(text) >3
+# except AssertionError:
+#     print('Слишком короткий текст')
+# Задача 1
+# text = [1,2,3,4,5,6,7,8,9]
+#
+# try:
+#     index = int(input('Введите индекс: '))
+#     if not 0 < index < 8:
+#         raise ValueError('введеное число вне диапазона ')
+#     print(f'Число по индексу {index}:{text[index]}')
+# except ValueError as exp:
+#     print('Надо быть внимательнее:',exp)
+#
+# lst = [1,2,3,4,5,6,7,8,9]
+#
+# try:
+#     index = int(input('Введите индекс: '))
+#     if -len(lst)< index<len(lst)-1:
+#        raise ValueError ('Индекс вне диапазона')
+#     res = lst[index]
+#     print(f'Число по индексу {index}:{res}')
+# except ValueError as exp:
+#     mess = exp.args
+#     if mess[0].startswith('invalid literal'):
+#         print(f'водить надо число')
+#     else:
+#         print(exp)
+#
+# loop =True
+# try:
+#     index = int(input('Введите индекс: '))
+#     print(f'Число по индексу {index}:{res}')
+#2 задача
+# while True:
+#     a = input('ведите первое число: ')
+#     b = input('ведите первое число: ')
+#
+#     if a.isdigit() and b.isdigit():
+#         if int(b) ==0:
+#             print('На ноль делить нельзя')
+#         else:
+#             print(int(a)/ int(b))
+#             break
+#     else:
+#         print('водить надо только числа')
+#
+
+while True:
+    a = input('ведите первое число: ')
+    b = input('ведите первое число: ')
+    try:
+        res = int(a)/int(b)
+    except ZeroDivisionError:
+        print('На ноль делить нельзя!')
+    except ValueError:
+        print(f'водить надо число')
+    else:
+        print(res)
+        break
