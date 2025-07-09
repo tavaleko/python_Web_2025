382837cfec21c694c7a6456a7a82df3b44cf12df
LCIMS1 <tye@mk.ru>
2025-07-09 14:37:18 +0300

    lesson_11 (full lesson) ООП (greedy quantifier);жадный и ленивый квантификатор.INIT. getter and setter

--- package_1/lib.py
+++ package_1/lib.py
@@ -13,3 +13,31 @@ class Car:
             print(f'Едем в {place} на {self.brand} {self.model} {self.color}')
         else:
             print('Двигатель не заведен, не едем')
+
+class Person:
+    def __init__(self, name='Bill',age=1):
+        # свойства (поля) класса
+        self._name = name# так правильно
+        self._age = age
+
+
+
+    def person_info(self):
+        print(f'Человек с именем: {self._name}. Возраст:{self._age}')
+
+    #setters
+    def set_name(self, new_name):
+        if new_name:
+            self._name = new_name
+
+
+    def set_age(self, new_age):
+        if 0< new_age <150:
+            self._age = new_age
+        else:
+            print('Некоректный возраст-',new_age)
+
+    # getter
+    def get_age(self):
+        return self._name
+
