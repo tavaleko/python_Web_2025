1a48573dd897fff112770a102d961fb5755d21de
LCIMS1 <tye@mk.ru>
2025-07-10 11:10:19 +0300

    lesson_12 (full lesson)ООП polymorphism

--- main.py
+++ main.py
@@ -1,50 +1,113 @@
+# ООП polymorphism полиморфизм
+# method override; operator overloading -перегрузка оператора
+# # когда мы вызываем принт и пишем 1+2 будет три это будет целое число потому что оператор + полиморфный
+# print(1+2)#3 -> int
+# print(1+2.0)#3.0-> float
+# print('abc'+'def')#abcdef-> str
+# print([1,2]+[3,4])#[1, 2, 3, 4] сложили списки
+# def func(x,y):
+#     return x+y
+# print(func(2,3.0))#5.0 -> float
+from multiprocessing.util import sub_debug
 
-print('mause house "ville"')
-# python enhanced proposal 8
-name = 'Сергей' # string
-surname = 'Петров'
-age = 27
-print('Привет,', name, surname, 'тебе', age, 'лет')
-temperatura = 27
-print('На улице температура,', temperatura, 'градусов.')
-name = input('как тебя зовут: ') # str
-print('Привет,', name)
-print('Приятно познакомиться')
-print()
 
-donut = int(input('стоимость пончика: '))
-cofee = int(input('Стоимость кофе: '))
-#  total = coffee + donut
-# int, float, str
-print('С вас', cofee + donut, 'руб.')
-donut = input('стоимость пончика: ')
-cofee = input('Стоимость кофе: ')
-#  total = coffee + donut
-# int, float, str
-print('С вас', int(cofee) + int(donut), 'руб.')
-donut = float(input('стоимость пончика: '))
-cofee = float(input('Стоимость кофе: '))
-#  total = coffee + donut
-# int, float, str
-print('С вас', cofee + donut, 'руб.')
-a = 35.999
-print(int(a))
-temp = round(a)
-temp = round(a,2)
-temp = round(a,0)
-print(temp)
-screen = 1280 //6
-print(screen)
-number = int(input('Введите число: '))
-print('Число', number, 'заканчивается на', number % 10)
-print('Число'+ str(number) + 'заканчивается на', number % 10)
-""" 
-у лукоморья дуб зеленый
-"""
-temp = """ 
-у лукоморья дуб зеленый
-"""
-number = int(input('Введите число: '))
-print( 'число',number, 'в степени 3 будет', number**3 )
-print( 'квадратный корень от', number, 'будет', number **(1/2))
-print(4/2)# всегда будет float
+#
+# def get_author():
+#     return set._author
+#
+#
+# class Book:
+#     def __init__(self, title,author):
+#         self.title = title
+#         self.author = author
+#
+#     def get_title(self):
+#         return self.get_title
+#
+#
+#     def get_author(self):
+#         return self.get_author
+#
+#
+# book = Book('Язык С++', 'Бьярн Страупструп')
+#
+# print(f'{book.get_title()},{get_author()}')
+
+# полиморфизм это спойство кода работать с разными типами данных
+# from math import pi
+PI = 3.14
+# class Circle:
+#     def __init__(self,radius):
+#         self.radius = radius
+#
+#     def perimetr(self):
+#         return 2 * PI * self.radius
+#
+#     def area(self):
+#         return PI*self.radius **2
+#
+# class Square:
+#     def __init__(self, side):
+#         self.side = side
+#
+#     def perimetr(self):
+#         return 4 * self.side
+#
+#     def area(self):
+#         return self.side ** 2
+#
+# def shape_info(shape):
+#     print(f'Площадь: {shape.area()}, Периметр: {shape.perimetr}')
+#
+#
+# s =Square(10)
+# shape_info(s)#Площадь: 100, Периметр: <bound method Square.perimetr of <__main__.Square object at 0x000002292A2DAC90>>
+#
+# cr = Circle(10)
+# shape_info(cr)#Площадь: 314.0, Периметр: <bound method Circle.perimetr of <__main__.Circle object at 0x000001D623ADAE40>>
+#
+# print(dir(s))#->'side'] все остальные параметры одинаковые
+# print(dir(cr))#->'radius'] все остальные параметры одинаковые
+# # утиная типизация у питона- Полиморфизм
+# class Rectangle:
+#     def __init__(self,width,height):
+#         self.width = width
+#         self.height = height
+#
+#     def perimetr(self):
+#         return 2 * self.width + 2 * self.height
+#
+#     def area(self):
+#         return self.width * self.height
+#
+#     def shape_info(shape):
+#         print(f'Площадь: {shape.area()}, Периметр: {shape.perimetr}')
+#
+# r = Rectangle(10)
+# shape_info(r)# не работает
+#
+# class Circle:
+#     def __init__(self):
+#         self.radius = radius
+#         self.radius = "Круг"
+#     def perimetr(self):
+#          return 2 * PI * self.radius
+#
+#     def area(self):
+#         return PI*self.radius **2
+# def shape_info(shape:object):
+#     print(f'Площадь{shape.get_name()}a: {shape.area()}')
+# #
+# # посмотри нужно копировать
+# isinstance(объект, тип) -> True
+# isinstance(объект, (тип1, тип2, типN)) -> True если не совпадет вернет False
+# rect,c,sqr = 'Прямоугольник', 'круг', 'квадрат'
+#
+# def shape_info(shape:object):
+#     if isinstance(shape,Circle):
+#         fig = c
+#     elif isinstance(shape,Rectangle):
+#         fig = rect
+#     elif isinstance(shape,Square):
+#         fig = sqr
+#     print(f'Площадь: {fig}a:{shape.area}, Периметр: {shape.perimetr()}')
