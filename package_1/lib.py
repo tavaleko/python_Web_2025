b361c440d8b4cee91a61abe8d69f6914bc909a3c
LCIMS1 <tye@mk.ru>
2025-07-11 10:09:04 +0300

    lesson_13 (full lesson)

--- package_1/lib.py
+++ package_1/lib.py
@@ -1,3 +1,189 @@
+# Книги
+class Book:
+    def __init__(self, title, author):
+        self._title = title
+        self._author = author
+
+    def get_title(self):
+        return self._title
+
+    def get_author(self):
+        return self._author
+
+# Весы
+class Balance:
+    def __init__(self):
+        self._right = 0
+        self._left = 0
+
+    if __name__ == '__main__':
+        print('для проверки работы весов запустите файл balance_demo.py')
+
+    def add_left(self, weight: int) -> None:
+        """
+        Добавляет вес в левую чашу
+        :param weight: вес, кладомый в левую чашу
+        :raises ValueError: если вес отрицательный
+        """
+        if weight < 0:
+            raise ValueError('Снятие веса не поддерживается в текущей версии')
+        self._left += weight
+
+    def add_right(self, weight: int) -> None:
+        """
+        Добавляет вес в левую чашу
+        :param weight: вес, кладомый в правую чашу
+        :raises ValueError: если вес отрицательный
+        """
+        if weight < 0:
+            raise ValueError('Снятие веса не поддерживается в текущей версии')
+        self._right += weight
+
+    def result(self) -> str:
+        """
+        Возвращает результат взвешивания, согласно заданию
+        :return: текстовое сообщение о том, какая чаша перевесила
+        """
+        return 'весы в равновесии' if self._is_balanced() \
+            else 'левая чаша перевесила' if self._left > self._right \
+            else 'правая чаша перевесила'
+
+    def reset(self) -> str:
+        """
+        Опустошает обе чаши весов
+        :return: сообщение о том, что весы обнулены
+        """
+        self._left = 0
+        self._right = 0
+        return 'весы сброшены'
+
+    def get_status(self) -> str:
+        """
+        Сообщает текущее состояние весов
+        :return: текстовое сообщение о весе на обеих чашах
+        """
+        return f'на левой чаше {self._left}, на правой чаше {self._right}'
+
+    def get_difference(self) -> int:
+        """
+        Возвращает разницу веса между чашами весов
+        :return: положительное число, если левая чаша тяжелее,
+                отрицательное число, если правая чаша тяжелее,
+                0, если весы в равновесии
+        """
+        return self._left - self._right
+
+    def get_difference_percentage(self) -> float:
+        """
+        Возвращает в процентах от веса более лёгкой чаши,
+        на сколько вторая чаша тяжелее.
+        :return: положительное число, если левая чаша тяжелее,
+                отрицательное число, если правая чаша тяжелее,
+                0, если весы в равновесии
+        """
+        if self._is_balanced():
+            return 0.0
+        havier_scale = max(self._left, self._right)
+        lighter_scale = min(self._left, self._right)
+        if lighter_scale == 0:
+            return float('inf') if self._left > self._right else -float('inf')
+        ratio = ((havier_scale / lighter_scale) - 1) * 100
+        return ratio if self._left > self._right else -ratio
+
+    def _is_balanced(self) -> bool:
+        """
+        Проверяет, находятся ли весы в равновесии
+        :return: ИСТИНУ, если весы в равновесии, ЛОЖЬ в противном случае
+        """
+        return self._left == self._right
+
+
+class Sorter:
+    def __init__(self):
+        self.words = []
+
+    def add_word(self, word):
+        self.words.append(word)
+
+    def result(self):
+        # список слов, отсортированный по длине
+        return sorted(self.words, key=lambda x: len(x), reverse=True)
+
+
+class Separator:
+    def __init__(self):
+        self.odd = []
+        self.even = []  # чётные
+
+    def add_num(self, num):
+        if num % 2:
+            self.odd.append(num)
+        else:
+            self.even.append(num)
+
+    def get_odd(self):
+        return self.odd
+
+    def get_even(self):
+        return self.even
+
+
+class Clicker:
+    def __init__(self):
+        self._counter = 0
+
+    def click(self):
+        self._counter += 1
+
+    def get_counter(self):
+        return self._counter
+
+    def reset(self):
+        self._counter = 0
+
+
+class Car:
+    counter = 0  # статичное свойство (счетчик машин)
+
+    def __init__(self, brand='Noname', model='NoModel', color='black'):
+        self.brand = brand  # 'Skoda'
+        self.model = model  # 'Octavia'
+        self.color = color  # 'red'
+        self.engine_on = False
+        Car.counter += 1
+
+    def start_engine(self):
+        self.engine_on = True
+
+    def drive_to(self, place):
+        if self.engine_on:
+            print(f'Едем в {place} на {self.brand} {self.model}')
+        else:
+            print('Двигатель не заведён, не едем')
+
+    @staticmethod
+    def get_counter():
+        return Car.counter
+
+class Student:
+    def __init__(self, name='Bill', univ=''):
+        # свойства (поля) класса
+        self._name = name
+        self._univercity = univ
+
+    def get_univercity(self):
+        return self._univercity
+
+
+class Employee:
+    def __init__(self, name='Bill', comp=''):
+        # свойства (поля) класса
+        self._name = name
+        self._company = comp
+
+    def get_company(self):
+        return self._company
+
 class Person:
     def __init__(self, name='Bill', age=1):
         # свойства (поля) класса
@@ -26,8 +212,13 @@ class Person:
         print(f'Человек с именем {self._name}. Возраст: {self._age}')
 
 
+def summ(a, b):
+    return a + b
 
-class Student:
-    pass
 
-class Employee:
+def diff(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    print('Это библиотека, а исполняемый - main.py')
