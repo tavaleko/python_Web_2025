b58980dcde237b898c08907ba5ec1d40deb265ca
LCIMS1 <tye@mk.ru>
2025-07-14 11:23:01 +0300

    lesson_14 (full lesson) java script

---
+++ js/sample.js
@@ -0,0 +1,37 @@
+// коментарий если закоментировать одну строку скрипт ample.js
+/*
+коментарий многострочный некоторые элементы языка //javascript.ru
+джава скрипт со слабой типизацией
+*/
+//var// видна везде устаревшая
+/*function sayHello(name) {
+    document.writeln("Вас зовут" + name);
+}//camel case
+
+//еременные (var или let)
+let name = prompt("Ваше имя: ");
+sayHello(name);//вызов функции*/
+// массивы
+// Массивы
+let colors = ["Красный", "Синий", "Голубой"];
+
+/*document.writeln("<h1>Цвета:</h1><ol>");
+// Цикл for
+for(let i=0; i<colors.length; i++) {
+    document.writeln("<li>" + colors[i] + "</li>");
+}
+document.write("</ol>")*/
+/*function changeColor() {
+const txt = document.getElementById('alive').style.color = 'red';
+
+}*/
+function changeColor() {
+const txt = document.getElementById('alive')
+if(txt.style.display ==='none') { // можно попробовать заменить none на block, inline
+txt.style.display = block;
+ } else{
+ txt.style.display = 'none'
+ }
+}
+//подключаюсь к элементу dom
+const txt = document.getElementById('alive').onclick = changeColor;
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 # это закладка
 # https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js необходимо для карусели
+#   <link rel="stylesheet" href="css/style.css"> обязательно поставить!!!!
