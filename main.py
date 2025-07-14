19d8b547ff9541d941fd2ae8f2e8e21b697faf1a
LCIMS1 <tye@mk.ru>
2025-07-14 15:40:41 +0300

    lesson_14 (full lesson) java script sroll, базы данных csv, zip, JSON

--- main.py
+++ main.py
@@ -110,19 +110,30 @@
 #         print(f'{k}: {v}')
 
 import json
+#
+# with open('dogs_.json', 'rt') as d:
+#     temp = d.read() # читаем файл как строку
+#     data = json.loads(temp)# строковое значение JSON
+#
+#
+# for i in range(len(data)):
+#     print(f'Питомец №{i+1}')
+#     for k,v in data[i].items():
+#         if type(v) == list:
+#             print(f'{k}:{', '.join(v)}')
+#         else:
+#             print(f'{k}: {v}')
 
-with open('dogs_.json', 'rt') as d:
-    temp = d.read() # читаем файл как строку
-    data = json.loads(temp)# строковое значение JSON
-
-
-for i in range(len(data)):
-    print(f'Питомец №{i+1}')
-    for k,v in data[i].items():
-        if type(v) == list:
-            print(f'{k}:{', '.join(v)}')
-        else:
-            print(f'{k}: {v}')
-
-
+d = {
+    'ананас': 300,
+    'банан': 400,
+    'яблоко': 120,
+    'груша': 280,
+}
+# запись на прямую в файл
+# with open('fruits.json', 'w', encoding='utf-8')as f:
+#    json.dump(d,f, indent=4)
+# вывод в виде строки
+data = json.dumps(d, indent=4)
+print(data)
 
