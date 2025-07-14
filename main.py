61e399d156f43a195c03334857e1249f2df4661d
LCIMS1 <tye@mk.ru>
2025-07-14 15:30:57 +0300

    lesson_14 (full lesson) java script sroll, базы данных csv, zip, JSON

--- main.py
+++ main.py
@@ -95,19 +95,34 @@
 #         print(f'{k}:{', '.join(v)}')
 #     else:
 #         print(f'{k}: {v}')
+#
+# import json
+#
+# with open('dogs.json', 'rt') as d:
+#     temp = d.read() # читаем файл как строку
+#     data = json.loads(temp)
+#
+# print(data)
+# for k, v in data.items():
+#     if type(v) == list:
+#         print(f'{k}:{', '.join(v)}')
+#     else:
+#         print(f'{k}: {v}')
 
 import json
 
-with open('dogs.json', 'rt') as d:
+with open('dogs_.json', 'rt') as d:
     temp = d.read() # читаем файл как строку
-    data = json.loads(temp)
+    data = json.loads(temp)# строковое значение JSON
 
-print(data)
-for k, v in data.items():
-    if type(v) == list:
-        print(f'{k}:{', '.join(v)}')
-    else:
-        print(f'{k}: {v}')
+
+for i in range(len(data)):
+    print(f'Питомец №{i+1}')
+    for k,v in data[i].items():
+        if type(v) == list:
+            print(f'{k}:{', '.join(v)}')
+        else:
+            print(f'{k}: {v}')
 
 
 
