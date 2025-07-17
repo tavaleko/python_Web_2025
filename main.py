1305fc08fe30669ece459943bfd973fb0b567426
LCIMS1 <tye@mk.ru>
2025-07-17 11:49:02 +0300

    lesson_17 (full lesson) jinja Loop and Index

--- main.py
+++ main.py
@@ -1,148 +1,66 @@
-# #########################
-# Введение во  Flask
-############################
-# удалить любую библиотеку pip uninstall  дальше название библиотеки
-# MVC -(Model View Controller)
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
+from openpyxl.styles.builtins import title
+from werkzeug.utils import  secure_filename
 import sqlite3
 app = Flask(__name__)
-
-# декоратор @ начинается
-@app.route('/')
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+ALLOWED_EXTENSIONS =['txt','pdf','zip','jpg','png']
+debug= False
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# шаблонизатор JINJA переменные циклы и тд
 @app.route('/index')
-def index(): # функция не должна повторятся!!!!!!!!! иначе ошибка назавания должны быть разные
-    print('Вызвана функция о index')
-    return 'Привет, Flask'# call back def  функция обратного пути
+def index():
+    params = {}
+    params['user'] = 'слушатель'
+    params['title'] = 'приветствие'
+    params['weather'] = 'Сегодня хорошая погода'
+    return render_template('index.html',
+                           **params)
 
-@app.route('/about')
-def about():
-    print('Вызвана функция о about')
-    return 'О нас' # функция обратного вызова возвращает только строку даже если нужно число оно должно быть в скобках
+@app.route('/upload', methods=['POST', 'GET'])
+def file_upload():
+    if request.method == 'GET':
+        with open('upload.html', 'r', encoding='utf-8') as html:
+            return html.read()
+    elif request.method == 'POST':
+        # print(request.files)
+        if 'file' not in request.files:
+            return 'Файл не был выбран!!!'
 
-@app.route('/countdown')
-def cd():
-    lst= [str(x) for x in reversed(range(10))]
-    lst.append('Полетели!')
-    return '<br>'.join(lst) # вернет строку через <br>
- # функция обратного вызова возвращает только строку даже если нужно число оно должно быть в скобках
-# статический контент
-@app.route('/image')
-def show_image():
-    return f'<img scr="{url_for('static', filename= 'images/map.jpg')}">'# или '<img scr="./static/images/map.jpg">'
-# для изображений видео музыка она должна храниться в специальной папке  именно так! static!
+        file = request.files['file']
 
-@app.route('/sample-page')
-def sample_page():
-    return f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <title>Картинка питона</title>
-    </head>
-    <body>
-    <img src = "{url_for('static', filename = 'images/piton.jpg')}" alt="python">
-    </body>
-    </html>
-"""
-@app.route('/sample-page2')
-def sample_page_2():
-    with open('temp2.html','r', encoding = 'utf-8') as html:
-        return html.read()
+        if file.filename == '':
+            return 'Файл не был выбран!!!'
 
-#  так делать нельзя
-# x=5
-# @app.route('/1')
-# def show_num():
-#     global x
-#     x += 1
-#     return str(x)
-
-# конвектор <string> по умолчанию строка
-@app.route('/greeting/<user>')
-def greeting(user):
-    return f'Привет! {user}'
-# конвектор <int:number> целое число
-
-@app.route('/greeting/<user>/<int:id_num>')
-def greeting_1(user, id_num):
-    return f'Привет! {user} с id ={id_num}'
-# конвектор <float:number> десятичная дробь
-# конвектор <path:p> может содержать слеши для указания пути
-# конвектор <uuid:id> строка индефикатор (16 байт в HEX- формате) ля оплаты по ссылке
+        if file and allowed_file(file.filename):
+            new_name = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_name))
+            return f'Файл {new_name} успешно загружен!'
+    return "Ошибка загрузки"
 
 
-@app.route('/greet-user/<int:id_num>')
-def greet_user_1(id_num):
-    con = sqlite3.connect('db/movies.sqlite')
-    cur = con.cursor()
-    query = f'SELECT name FROM users WHERE trip_id={id_num}'
-    response = cur.execute(query)
-    result = response.fetchone()
-    cur.close()
-    con.close()
-    return str(result[0])
-
-@app.route('/get-user-2/<int:id_num>')
-def get_user(id_num):
-    con = sqlite3.connect('db/movies.sqlite')
-    cur = con.cursor()
-    query = f'SELECT name, city FROM users WHERE trip_id={id_num}'
-    response = cur.execute(query)
-    result = response.fetchone()
-    # print(result)
-    name, city = result
-    cur.close()
-    con.close()
-    return f'''<table border="1">
-    <tr>
-    <td>ФИО</td>
-    <td>Город</td>
-    </tr>
-    <tr>
-    <td>{name}</td>
-    <td>{city}</td>
-    </tr>
-    </table>'''
+@app.route('/numbers')
+def odd_even():
+    return render_template('numbers.html',
+                           title='Чет-нечёт', number=2)
 
 
-@app.route('/get-user-1')
-@app.route('/get-user-1/<int:id_num>')
-def get_user_2(id_num=None):
+@app.route('/deals')
+def printlist():
+    deal = ['Помыть посуду', 'Выгулять собаку',
+            'Снять показания счётчика', 'Сходить в магазин']
+    return render_template('printlist.html', deals=deal)
 
-    if id_num is None:
-        return 'Нет номера записи'
-    con = sqlite3.connect('db/movies.sqlite')
-    cur = con.cursor()
-    query = f'SELECT name FROM users WHERE trip_id={id_num}'
-    response = cur.execute(query)
-    result = response.fetchone()
-    cur.close()
-    con.close()
-    return str(result[0])
-
-
-@app.route('/form-test', methods=['POST','GET'])
-def form_test():
-    if request.method =='GET':
-        with open('form-test','r', encoding='utf-8') as html:
-            return  html.read()
-    elif request.method =='POST':
-        # print(request.form ['gender'])
-        # print(request.form['email'])
-        # print(request.form['about'])
-        # print(request.form['accept'])
-        print(request.form)
-        return 'Форма успешно отправлена'
-# Методы
+@app.route('/queue')
+def queue():
+    # loop index 0 номер итерации начинается с 0
+    # loop first True  если этерация первая
+    # loop last True если итерация последняя
+    # loop index номер итерации начинается с 1
+    return render_template('vars.html', title='Стоим в очереди')
 
 if __name__ == '__main__':
-    app.run(host = 'localhost', port=5000, debug=True) # lockalhost адрес 127.0.0.1
+    app.run(host = 'localhost', port=5000, debug=debug) # lockalhost адрес 127.0.0.1
     # debug=True чтобы не было разницы между приложением и браузером
-################################################################
-
-
-# методы GET  запрашивает данные(read)
-# метод POST отправляет данные на сервер(submit)
-# метод PUT принудительно заменяет все на сервере из контекста запроса(заменить)
-# метод DELETE  удаляет данные
-# метод PATСH частично изменяет данные
